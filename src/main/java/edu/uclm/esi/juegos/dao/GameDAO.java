666839253748b819c0b9cc8dc42c92dadfdad32e
Pablo12uclm <115165900+Pablo12uclm@users.noreply.github.com>
2024-05-28 19:17:05 +0200

    Delete src directory

--- src/main/java/edu/uclm/esi/juegos/dao/GameDAO.java
+++
@@ -1,8 +0,0 @@
-package edu.uclm.esi.juegos.dao;
-import edu.uclm.esi.juegos.entities.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GameDAO extends JpaRepository<Game, Long> {
-}
